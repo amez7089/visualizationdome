5c34ed5e2154b7d0dfee1247e840a6c35e5d227e
zhouchuqi <1269758616@qq.com>
2018-12-20 16:48:38 +0800

    新建python3学习框架

---
+++ PyQt_dome/PyQt_test1.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+# @Time     :2018/12/20 14:52
+# @Author   :ZhouChuqi
---
+++ tkinter_dome/tkinter_test1.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+# @Time     :2018/12/20 16:47
+# @Author   :ZhouChuqi
